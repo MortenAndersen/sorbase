f1f5a8154a7ac30635de4e9ed04512c9fe5d5d7f
Morten Andersen <morten@hjemmesider.dk>
2020-09-17 13:19:35 +0200

    sor-menu

--- sidebar.php
+++ sidebar.php
@@ -2,6 +2,7 @@
 <div class="background background-footer">
     <div class="l-wrap">
         <footer class="page-footer l-footer--content grid-con" role="contentinfo">
+        	<?php get_template_part( 'sor-menu' ); ?>
         	<?php dynamic_sidebar( 'sidebar-footer' ); ?>
         	<?php get_template_part( 'template/navigation/nav', 'footermenu' ); ?>
     </div>
---
+++ sor-menu.php
@@ -0,0 +1,10 @@
+<nav class="sor-footer-menu widget footer-widget">
+	<ul class="menu">
+		<li><a href="https://www.soroptimistinternational.org/" target="_blank">SI</a></li>
+		<li><a href="http://www.soroptimisteurope.org/" target="_blank">SI Europa</a></li>
+		<li><a href="https://www.soroptimist-danmark.dk/">SI Danmark</a></li>
+		<li><a href="https://www.soroptimist-danmark.dk/danske-klubber/">Danske klubber</a></li>
+		<li><a href="https://www.soroptimist-danmark.dk/links/">Links</a></li>
+		<li><a href="#">Login</a></li>
+	</ul>
+</nav>
