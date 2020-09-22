6a4a9af92a47835ff2d10d943f2abfc39880b946
Morten Andersen <morten@hjemmesider.dk>
2020-09-22 15:13:58 +0200

    user menu

--- sor-menu.php
+++ sor-menu.php
@@ -1,10 +1,10 @@
 <nav class="sor-footer-menu widget footer-widget">
+	<h5 class="widget-title">Menu</h5>
 	<ul class="menu">
 		<li><a href="https://www.soroptimistinternational.org/" target="_blank">SI</a></li>
 		<li><a href="http://www.soroptimisteurope.org/" target="_blank">SI Europa</a></li>
 		<li><a href="https://www.soroptimist-danmark.dk/">SI Danmark</a></li>
 		<li><a href="https://www.soroptimist-danmark.dk/danske-klubber/">Danske klubber</a></li>
 		<li><a href="https://www.soroptimist-danmark.dk/links/">Links</a></li>
-		<li><a href="#">Login</a></li>
 	</ul>
 </nav>
