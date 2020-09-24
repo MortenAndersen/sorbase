ca7e3705853f0845c0ae4cd1be178d032cf3d117
Morten Andersen <morten@hjemmesider.dk>
2020-09-24 14:49:01 +0200

    aside left filer

--- page-left-aside.php
+++ page-left-aside.php
@@ -7,6 +7,7 @@
   <div class="l-wrap l-main--content simple-grid-con space-between">
     <div class="main simple-grid-item-main order-2">
       <?php get_template_part( 'template/page/page', 'loop' ); ?>
+      <?php simpleTheme_download(); ?>
     </div>
     <aside class="aside-left simple-grid-item-aside order-1">
     	<?php get_template_part( 'template/page/aside', 'left' ); ?>
