9f5cd5d1d3eb9956694bfc71ac78a01c29e0d118
Morten Andersen <morten@hjemmesider.dk>
2020-10-05 18:42:48 +0200

    danske klubber

---
+++ page-danske-klubber.php
@@ -0,0 +1,115 @@
+<?php get_header(); ?>
+<div id="content" class="background background-main">
+    <div class="l-wrap l-main--content">
+        <div class="main">
+        	<?php get_template_part( 'template/page/page', 'loop' ); ?>
+
+<div class="flex-con g4 danske-klubber-con">
+	<div class="flex-item">
+		<h3>Region 1</h3>
+		<ul class="danske-kubber">
+			<li class="site-20"><a href="https://www.soroptimist-danmark.dk/hjoerring">Hjørring</a></li>
+			<li class="site-31"><a href="https://www.soroptimist-danmark.dk/mariagerfjord">Mariager Fjord</a></li>
+			<li class="site-37"><a href="https://www.soroptimist-danmark.dk/randers">Randers</a></li>
+			<li class="site-43"><a href="https://www.soroptimist-danmark.dk/skagen">Skagen</a></li>
+			<li class="site-45"><a href="https://www.soroptimist-danmark.dk/skive">Skive</a></li>
+			<li class="site-2"><a href="https://www.soroptimist-danmark.dk/aalborg">Aalborg</a></li>
+		</ul>
+	</div>
+
+	<div class="flex-item">
+		<h3>Region 2</h3>
+		<ul class="danske-kubber">
+			<li class="site-9"><a href="https://www.soroptimist-danmark.dk/brande">Brande</a></li>
+			<li class="site-14"><a href="https://www.soroptimist-danmark.dk/grenaa">Grenå</a></li>
+			<li class="site-18"><a href="https://www.soroptimist-danmark.dk/herning">Herning</a></li>
+			<li class="site-23"><a href="https://www.soroptimist-danmark.dk/ikast">Ikast</a></li>
+			<li class="site-35"><a href="https://www.soroptimist-danmark.dk/odder">Odder</a></li>
+			<li class="site-39"><a href="https://www.soroptimist-danmark.dk/ringkoebing">Ringkøbing</a></li>
+			<li class="site-42"><a href="https://www.soroptimist-danmark.dk/silkeborg">Silkeborg</a></li>
+			<li class="site-44"><a href="https://www.soroptimist-danmark.dk/skanderborg">Skanderborg</a></li>
+			<li class="site-3"><a href="https://www.soroptimist-danmark.dk/skjerntarm">Skjern Tarm</a></li>
+			<li class="site-54"><a href="https://www.soroptimist-danmark.dk/aarhus">Århus</a></li>
+		</ul>
+	</div>
+
+	<div class="flex-item">
+		<h3>Region 3</h3>
+		<ul class="danske-kubber">
+			<li class="site-4"><a href="https://www.soroptimist-danmark.dk/assens">Assens</a></li>
+			<li class="site-7"><a href="https://www.soroptimist-danmark.dk/bogensenordfyn">Bogense-Nordfyn</a></li>
+			<li class="site-12"><a href="https://www.soroptimist-danmark.dk/fredericia">Fredericia</a></li>
+			<li class="site-22"><a href="https://www.soroptimist-danmark.dk/horsens">Horsens</a></li>
+			<li class="site-26"><a href="https://www.soroptimist-danmark.dk/kerteminde">Kerteminde</a></li>
+			<li class="site-32"><a href="https://www.soroptimist-danmark.dk/nyborg">Nyborg</a></li>
+			<li class="site-36"><a href="https://www.soroptimist-danmark.dk/odense">Odense</a></li>
+			<li class="site-51"><a href="https://www.soroptimist-danmark.dk/vejle">Vejle</a></li>
+		</ul>
+	</div>
+
+	<div class="flex-item">
+		<h3>Region 4</h3>
+		<ul class="danske-kubber">
+			<li class="site-11"><a href="https://www.soroptimist-danmark.dk/esbjerg">Esbjerg</a></li>
+			<li class="site-15"><a href="https://www.soroptimist-danmark.dk/grindstedbillund">Grindsted Billund</a></li>
+			<li class="site-16"><a href="https://www.soroptimist-danmark.dk/haderslev">Haderslev</a></li>
+			<li class="site-27"><a href="https://www.soroptimist-danmark.dk/kolding">Kolding</a></li>
+			<li class="site-38"><a href="https://www.soroptimist-danmark.dk/ribe">Ribe</a></li>
+			<li class="site-41"><a href="https://www.soroptimist-danmark.dk/roeddinggramtoftlund">Rødding Gram Toftlund</a></li>
+			<li class="site-47"><a href="https://www.soroptimist-danmark.dk/soenderborg">Sønderborg</a></li>
+			<li class="site-48"><a href="https://www.soroptimist-danmark.dk/toender">Tønder</a></li>
+			<li class="site-49"><a href="https://www.soroptimist-danmark.dk/varde">Varde</a></li>
+			<li class="site-50"><a href="https://www.soroptimist-danmark.dk/vejen">Vejen</a></li>
+			<li class="site-53"><a href="https://www.soroptimist-danmark.dk/aabenraa">Åbenrå</a></li>
+		</ul>
+	</div>
+
+	<div class="flex-item">
+		<h3>Region 5</h3>
+		<ul class="danske-kubber">
+			<li class="site-6"><a href="https://www.soroptimist-danmark.dk/birkeroed">Birkerød</a></li>
+			<li class="site-17"><a href="https://www.soroptimist-danmark.dk/helsinge">Helsinge</a></li>
+			<li class="site-19"><a href="https://www.soroptimist-danmark.dk/hilleroed">Hillerød</a></li>
+		</ul>
+	</div>
+
+	<div class="flex-item">
+		<h3>Region 6</h3>
+		<ul class="danske-kubber">
+			<li class="site-5"><a href="https://www.soroptimist-danmark.dk/avedoerehvidovre">Avedøre Hvidovre</a></li>
+			<li class="site-8"><a href="https://www.soroptimist-danmark.dk/bornholmleonora">Bornholm Leonora</a></li>
+			<li class="site-10"><a href="https://www.soroptimist-danmark.dk/broendby">Brøndby</a></li>
+			<li class="site-13"><a href="https://www.soroptimist-danmark.dk/gentofte">Gentofte</a></li>
+			<li class="site-55"><a href="https://www.soroptimist-danmark.dk/glostrup">Glostrup</a></li>
+			<li class="site-25"><a href="https://www.soroptimist-danmark.dk/kastruptaarnby">Kastrup Tårnby</a></li>
+			<li class="site-28"><a href="https://www.soroptimist-danmark.dk/koebenhavn">København</a></li>
+			<li class="site-29"><a href="https://www.soroptimist-danmark.dk/koebenhavnathene">København Athene</a></li>
+		</ul>
+	</div>
+
+	<div class="flex-item">
+		<h3>Region 7</h3>
+		<ul class="danske-kubber">
+			<li class="site-21"><a href="https://www.soroptimist-danmark.dk/holbaek">Holbæk</a></li>
+			<li class="site-24"><a href="https://www.soroptimist-danmark.dk/kalundborg">Kalundborg</a></li>
+			<li class="site-46"><a href="https://www.soroptimist-danmark.dk/slagelse">Slagelse</a></li>
+		</ul>
+	</div>
+
+	<div class="flex-item">
+		<h3>Region 8</h3>
+		<ul class="danske-kubber">
+			<li class="site-30"><a href="https://www.soroptimist-danmark.dk/lolland">Lolland</a></li>
+			<li class="site-33"><a href="https://www.soroptimist-danmark.dk/nykoebingfalster">Nykøbing Falster</a></li>
+			<li class="site-34"><a href="https://www.soroptimist-danmark.dk/naestved">Næstved</a></li>
+			<li class="site-40"><a href="https://www.soroptimist-danmark.dk/ringsted">Ringsted</a></li>
+			<li class="site-52"><a href="https://www.soroptimist-danmark.dk/vordingborg">Vordingborg</a></li>
+		</ul>
+	</div>
+</div>
+
+        	<?php simpleTheme_download(); ?>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
